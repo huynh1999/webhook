3085766b802ff55e204fbd9b554749f5df442d9e
huynh1999 <55187798+huynh1999@users.noreply.github.com>
2020-03-01 00:11:39 +0700

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -7,6 +7,13 @@ public class Controller {
     @GetMapping("/post")
     public String post(@RequestParam("hub.challenge") String challenge,@RequestParam("hub.verify_token")String token)
     {
+        System.out.println(challenge);
+        System.out.println(token);
         return challenge;
+    }
+    @PostMapping("/post")
+    public String getPost(@RequestBody String body)
+    {
+        return body;
     }
 }
